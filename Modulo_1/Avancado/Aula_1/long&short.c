c3e1caf5e076b55563c857612643db1715b9dd40
Tatiane <202503794529@alunos.unifanor.edu.br>
2025-05-30 18:23:39 -0300

    Create long&short.c

---
+++ Modulo_1/Avancado/Aula_1/long&short.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+int main()
+{
+  int semSinal = 300000000;
+
+  unsingned long long int comSinal = 30000000000;
+  long duble comSinal = 3.144244241312212312312;
+
+  printf("Sem Sinal : %d.\n", semSinal);
+  //printf("comSinal: %u, comSinal;
+  printf("Com Sinal : %lld", comSinal);
+
+  printf("\nCom Sinal : %21lf", comSinal);
+  return 0;
+}

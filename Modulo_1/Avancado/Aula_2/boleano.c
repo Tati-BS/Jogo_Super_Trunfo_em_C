8e12d2e18d589cd25cc97a6e43a51c30ae88561c
Tatiane <202503794529@alunos.unifanor.edu.br>
2025-05-30 18:25:30 -0300

    Create boleano.c

---
+++ Modulo_1/Avancado/Aula_2/boleano.c
@@ -0,0 +1,18 @@
+#incluede <studio.h>
+
+int main()
+{
+  int a = 10, b =10.2;
+  printf("a > b: %d\n", a > b);
+  printf("a < b: %d\n", a < b);
+  printf("a == b: %d\n", a == b);
+  printf("a != b: %d\n", a != b);
+
+if ((int) a ==b)
+{
+  printf("Olá");
+}
+
+return 0;
+
+}

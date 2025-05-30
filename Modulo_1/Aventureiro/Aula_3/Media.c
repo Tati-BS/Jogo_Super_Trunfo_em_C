b153741aaf5531ec534a5239edbc567f9f92cba2
Tatiane <202503794529@alunos.unifanor.edu.br>
2025-05-30 19:00:58 -0300

    Create Media.c

---
+++ Modulo_1/Aventureiro/Aula_3/Media.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+int main()
+{
+  float nota1, nota2, nota3;
+  float media;
+
+printf("Me Diga a Nota do primeiro Brimestre: ");
+scanf("%f", &nota1);
+
+printf("Me Diga a Nota do segundo Brimestre: ");
+scanf("%f", &nota2);
+
+printf("Me Diga a Nota do terceiro Brimestre: ");
+scanf("%f", &nota3);
+
+media = /*(float)*/ (nota1 + nota2 + nota) / 3;
+printf("A média das suas notas é %.2f", media);
+
+return 0;
+}

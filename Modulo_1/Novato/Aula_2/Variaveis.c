8c27216455efb4c4a9f43f051bac925858f62da9
Tatiane <202503794529@alunos.unifanor.edu.br>
2025-05-30 18:39:13 -0300

    Create Variaveis.c

---
+++ Modulo_1/Novato/Aula_2/Variaveis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    // dados primitivos
+
+    int idades = 20;
+    float metros = 1.5;
+    char  letra = 'A';
+    char animal[20] = "cavalo";
+    
+    printf("%i + %4f + %c + %s", idades, metros, letra, animal);
+
+    return 0;
+ 
+ 
+ 
+ 
+}

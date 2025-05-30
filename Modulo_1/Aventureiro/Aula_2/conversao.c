122d66ea2b025a0db3bd71bf30c6a09fbf21ec06
Tatiane <202503794529@alunos.unifanor.edu.br>
2025-05-30 18:54:44 -0300

    Create conversao.c

---
+++ Modulo_1/Aventureiro/Aula_2/conversao.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+/*
+    float A = 5.5;
+    float B = 3.32;
+    float soma = A + B;
+    float subtracao = A - B;
+    float multiplicacao = A * B;
+    float divisao =  A / B;
+
+    printf("soma: %.2f", soma);
+    printf("\nsubtração: %.2f", subtracao);
+    printf("\nmultiplicação: %.2f", multiplicacao);
+    printf("\ndivisão: %.2f", divisao);
+*/
+    int A = 5;
+    float B = 3.62;
+    float resultado = (float) A / B;
+
+    printf("soma: %f", resultado);
+
+    return 0;
+
+}
